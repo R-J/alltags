d27a980df22be15901e23bd1111f15b49a95dca9
R-J <robin.jurinka@gmx.net>
2018-05-25 13:07:22 +0200

    Initial commit

--- class.alltags.plugin.php
+++ class.alltags.plugin.php
@@ -2,21 +2,33 @@
 
 class AllTagsPlugin extends Gdn_Plugin {
     public function vanillaController_allTags_create($sender, $args) {
+saveToCOnfig('alltags.SortOrder', 'Count');
+        // Prepare Vanilla page.
         $sender->masterView();
-        // $sender->SelfUrl = self::SHORT_ROUTE;
         foreach (c('Modules.Vanilla.Panel') as $module) {
             if ($module != 'MeModule') {
                 $sender->addModule($module);
             }
         }
-
         $sender->title(t('All Tags'));
-
-        // This sets the breadcrumb to our current page.
         $sender->setData(
             'Breadcrumbs',
             [['Name' => t('All Tags'), 'Url' => 'vanilla/alltags']]
         );
+
+        if (c('alltags.SortOrder', 'Name') == 'Name') {
+            $orderBy = 'FullName';
+            $orderDirection = 'asc';
+        } else {
+            $orderBy = 'CountDiscussions';
+            $orderDirection = 'desc';
+        }
+        $tags = TagModel::instance()->get($orderBy, $orderDirection)->resultArray();
+
+        $sender->setData([
+            'OrderBy' => $orderBy,
+            'Tags' => $tags
+        ]);
 
         $sender->render('alltags', '', 'plugins/alltags');
     }
--- views/alltags.php
+++ views/alltags.php
@@ -1,1 +1,18 @@
-<?
+<?php defined('APPLICATION') or die;
+$format = t(
+    'Alltags.Format',
+    '%1$s<span class="Aside"><span class="Count">%2$d</span></span>'
+);
+?>
+<ul class="PanelInfo">
+<?php foreach ($this->data('Tags') as $tag): ?>
+    <li class="Item"><?php echo anchor(
+        sprintf(
+            $format,
+            Gdn_Format::plainText($tag['FullName']),
+            $tag['CountDiscussions']
+        ),
+        url('discussions/tagged/'.Gdn_Format::plainText($tag['Name']))
+    ) ?></li>
+<?php endforeach ?>
+</ul>
