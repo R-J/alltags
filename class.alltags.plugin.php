6a466d5df233686d014a1d79f6edcdfd20e71288
R-J <robin.jurinka@gmx.net>
2018-05-26 10:25:14 +0200

    Add PluginInfo to plugin file and set minimum Vanilla version to 2.5 :-(

--- class.alltags.plugin.php
+++ class.alltags.plugin.php
@@ -1,8 +1,28 @@
 <?php
 
+$PluginInfo['alltags'] = [
+   'Name' => 'All Tags',
+    'Description' => 'Add a page to Discussion Filter menu which shows all tags',
+    'Version' => '0.1.0',
+    'License' => 'MIT',
+    'Type' => 'addon',
+    'MobileFriendly': true,
+    'HasLocale': true,
+    'PluginUrl' => 'https://github.com/r-j/alltags',
+    'Author' => 'Robin Jurinka',
+    'AuthorUrl' => 'https://open.vanillaforums.com/profile/r_j',
+    'RequiredApplications' => ['Vanilla' => '2.5'],
+];
+
 class AllTagsPlugin extends Gdn_Plugin {
-    public function vanillaController_allTags_create($sender, $args) {
-saveToCOnfig('alltags.SortOrder', 'Name');
+    /**
+     * Create a new page containing a list of all tags.
+     *
+     * @param VanillaController $sender Instance of the calling class.
+     *
+     * @return void.
+     */
+    public function vanillaController_allTags_create($sender) {
         // Prepare Vanilla page.
         $sender->masterView();
         foreach (c('Modules.Vanilla.Panel') as $module) {
@@ -10,12 +30,8 @@ saveToCOnfig('alltags.SortOrder', 'Name');
                 $sender->addModule($module);
             }
         }
-        $sender->title(t('All Tags'));
-        $sender->setData(
-            'Breadcrumbs',
-            [['Name' => t('All Tags'), 'Url' => 'vanilla/alltags']]
-        );
 
+        // Determine sort order for list of tags.
         if (c('alltags.SortOrder', 'Name') == 'Name') {
             $orderBy = 'FullName';
             $orderDirection = 'asc';
@@ -23,17 +39,26 @@ saveToCOnfig('alltags.SortOrder', 'Name');
             $orderBy = 'CountDiscussions';
             $orderDirection = 'desc';
         }
-        $tags = TagModel::instance()->get($orderBy, $orderDirection)->resultArray();
 
+        // Pass data to view.
         $sender->setData([
-            'OrderBy' => $orderBy,
-            'Tags' => $tags
+            'Title' => t('All Tags'),
+            'Breadcrumbs' => [['Name' => t('All Tags'), 'Url' => 'vanilla/alltags']],
+            'Tags' => TagModel::instance()->get($orderBy, $orderDirection)->resultArray()
         ]);
 
         $sender->render('alltags', '', 'plugins/alltags');
     }
 
+    /**
+     * Add link to Discussion Filter module.
+     *
+     * @param Gdn_Controller $sender Instance of the calling class.
+     *
+     * @return void.
+     */
     public function base_afterDiscussionFilters_handler($sender) {
+        // Check if menu entry needs to be highlighted.
         if ($sender->SelfUrl == 'vanilla/alltags') {
             $css = ' class="Active"';
         } else {
