3c7c90b4739fde71027ef026fb6a2467d854f432
R-J <robin.jurinka@gmx.net>
2019-12-08 02:17:23 +0100

    Remove unnecessary url() usage from within an anchor call. Replace t() calls

--- views/alltags.php
+++ views/alltags.php
@@ -1,25 +1,29 @@
 <?php defined('APPLICATION') or die;
-$format = t(
+$format = Gdn::translate(
     'Alltags.Format',
     '%1$s<span class="Aside"><span class="Count">%2$d</span></span>'
 );
 ?>
 <style>
-.AllTagsList {width: 200px;}
+  .AllTagsList {width: 200px}
 </style>
 <h1><?= $this->title() ?></h1>
-<h2 class="sr-only"><?= t('Tag List') ?></h2>
+<h2 class="sr-only"><?= Gdn::translate('Tag List') ?></h2>
 <div class="PanelColumn">
-<ul class="AllTagsList PanelInfo">
-<?php foreach ($this->data('Tags') as $tag): ?>
-    <li><?php echo anchor(
-        sprintf(
-            $format,
-            Gdn_Format::plainText($tag['FullName']),
-            $tag['CountDiscussions']
-        ),
-        url('discussions/tagged/'.Gdn_Format::plainText($tag['Name']))
-    ) ?></li>
-<?php endforeach ?>
-</ul>
+    <ul class="AllTagsList PanelInfo">
+    <?php foreach ($this->data('Tags') as $tag): ?>
+        <li>
+        <?php
+        echo anchor(
+            sprintf(
+                $format,
+                Gdn_Format::plainText($tag['FullName']),
+                $tag['CountDiscussions']
+            ),
+            '/discussions/tagged/'.Gdn_Format::plainText($tag['Name'])
+        );
+        ?>
+        </li>
+    <?php endforeach ?>
+    </ul>
 </div>
